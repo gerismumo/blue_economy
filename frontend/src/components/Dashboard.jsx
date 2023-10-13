7b24bf47b7552c033269221ab0fff2f33e770291
gerismumo <geraldmumo6@gmail.com>
2023-10-13 12:52:08 +0300

    dashboard css changes

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -10,6 +10,7 @@ function Dashboard() {
     const[usersList, setUsersList] = useState([]);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [editingUser, setEditingUser] = useState(null);
+    const[errorMessages, setErrorMessages] = useState({});
 
     // const onDrop = (acceptedFiles) => {
     //     acceptedFiles.forEach((file) => {
@@ -196,19 +197,78 @@ function Dashboard() {
         }
     
         const [checkedBoxes, setCheckedBoxes] = useState([]);
-    
-            const handleCheckboxChangeClick = (event) => {
+
+            const handleCheckboxClick = (event) => {
                 const { value } = event.target;
-                const updatedCheckedBoxes = checkedBoxes.includes(value)
-                ? checkedBoxes.filter((item) => item !== value)
-                : [...checkedBoxes, value];
-    
-                setCheckedBoxes(updatedCheckedBoxes);
+                setCheckedBoxes((prevCheckedBoxes) => {
+                if (prevCheckedBoxes.includes(value)) {
+                    return prevCheckedBoxes.filter((item) => item !== value);
+                } else {
+                    return [...prevCheckedBoxes, value];
+                }
+                });
             };
+
+            const resetFormFields = () => {
+                setFormData({
+                    email: '',
+                    name: '',
+                    occupation: '',
+                    company: '',
+                    phoneNumber: '',
+                    industry: '',
+                    describeYourProduct: '',
+                }); 
+            }
 
             const handleSubmit = async(e) => {
                 e.preventDefault();
                
+                const errors = {};
+                if(!formData.email) {
+                    errors.email = 'Please enter a valid email'
+                }
+                if(!formData.name) {
+                    errors.name = 'Please enter a valid name'
+                }
+                if(!formData.occupation) {
+                    errors.occupation = 'Please enter your occupation'
+                }
+                if(!formData.company) {
+                    errors.company = 'Please enter company name'
+                }
+                if(!formData.phoneNumber) {
+                    errors.phoneNumber = 'Please enter phone number'
+                }
+                if(!formData.industry) {
+                    errors.industry = 'Please enter Industry name'
+                }
+                if(!formData.attendLastYear) {
+                    errors.attendLastYear = 'Please select an option';
+                }
+                if(!formData.areaOfInterests) {
+                    errors.areaOfInterests = 'Please select an option';
+                }
+                if(!formData.joinMailList) {
+                    errors.joinMailList = 'Please select an option';
+                }
+                if(!formData.JoinAs) {
+                    errors.JoinAs = 'Please select an option';
+                }
+                if(!formData.describeYourProduct) {
+                    errors.describeYourProduct = 'Please enter a description';
+                }
+                if(!formData.categoryFall) {
+                    errors.categoryFall = 'Please select a option';
+                }
+
+                if(checkedBoxes.length === 0) {
+                    errors.checkBoxError = "Please select at least one option."
+                }
+                if(Object.keys(errors).length > 0) {
+                    setErrorMessages(errors);
+                    return;
+                }
                 const requestData = {
                     email: formData.email,
                     name: formData.name,
@@ -242,6 +302,13 @@ function Dashboard() {
                     const result = await response.json();
                     console.log('data being submitted', result);
                     // setUsersList((prevUsersList) => [...prevUsersList, result.data]);
+                    setCheckedBoxes([]);
+                    resetFormFields();
+                    document.getElementById('attend').selectedIndex = 0;
+                    document.getElementById('interests-area').selectedIndex = 0;
+                    document.getElementById('join-mail').selectedIndex = 0;
+                    document.getElementById('join-summit').selectedIndex = 0;
+                    document.getElementById('category-Fall').selectedIndex = 0;
                     setAddFormOpen(false);
                   } catch (error) {
                     console.error('Error in registering User', error);
@@ -338,8 +405,8 @@ function Dashboard() {
                             <th>How will you be joining this year's summit?</th>
                             <th>Describe your product or the services that you offer?</th>
                             <th>Which category do you fall in?</th>
-                            <th>Delete</th>
                             <th>Edit</th>
+                            <th>Delete</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -362,8 +429,9 @@ function Dashboard() {
                                 <td>{user.join_as}</td>
                                 <td>{user.describe_product}</td>
                                 <td>{user.category_fall}</td>
-                                <td><button onClick={()=>handleDeleteUser(user.user_id)}>Delete</button></td>
                                 <td><button onClick={()=>handleEditUser(user)}>Edit</button></td>
+                                <td><button onClick={()=>handleDeleteUser(user.user_id)}>Delete</button></td>
+                                
                             </tr>
                         ))
                     )}
@@ -521,7 +589,9 @@ function Dashboard() {
                             <option value="StartUp(KES 5000)">StartUp(KES 5000)</option>
                             <option value="Corporate Institution (KES 30,000)">Corporate Institution (KES 30,000)</option>
                         </select>
-                        <button type='submit'>Submit</button>
+                        <div className="modal-button">
+                            <button type='submit'>Submit</button>
+                        </div> 
                 </form>
             </div>
             </div>
@@ -536,6 +606,7 @@ function Dashboard() {
                         {/* Place your form code here */}
                         <form onSubmit={handleSubmit}>
                     <label htmlFor="email"  >Email</label>
+                    <span>{errorMessages.email}</span>
                     <input 
                     type='email' 
                     placeholder='johndoe@gmail.com' 
@@ -544,36 +615,42 @@ function Dashboard() {
                     onChange={handleChange}
                     />
                     <label htmlFor="name">Full name</label>
+                    <span>{errorMessages.name}</span>
                     <input type='text'
                     name='name'
                      value={formData.name}
                      placeholder='John Doe' 
                      onChange={handleChange}/>
                     <label htmlFor="occupation">Designation/Occupation/Role</label>
+                    <span>{errorMessages.occupation}</span>
                     <input type="text" 
                     name='occupation'
                     value={formData.occupation}
                     onChange={handleChange}
                     />
                     <label htmlFor="company">Company/Organisation</label>
+                    <span>{errorMessages.company}</span>
                     <input type='text'
                     name='company'
                     value={formData.company}
                     onChange={handleChange}
                     />
                     <label htmlFor="phoneNumber">Phone Number</label>
+                    <span>{errorMessages.phoneNumber}</span>
                     <input type="tel" 
                     name='phoneNumber'
                     value={formData.phoneNumber}
                     onChange={handleChange} 
                     />
                     <label htmlFor="form">Which industry are you in?</label>
+                    <span>{errorMessages.industry}</span>
                     <input type="text" 
                     name='industry'
                     value={formData.industry}
                     onChange={handleChange}
                     />
                     <label htmlFor="form">How did you hear about the event?</label>
+                    <span>{errorMessages.checkBoxError}</span>
                     <div className="select-checkboxes">
                     <label className="checkbox-label">
                         <input
@@ -582,7 +659,7 @@ function Dashboard() {
                             className='event-CheckBox'
                             value="Email"
                             checked={checkedBoxes.includes('Email')}
-                            onChange={handleCheckboxChangeClick}
+                            onChange={handleCheckboxClick}
                         />
                         Email
                     </label>
@@ -595,7 +672,7 @@ function Dashboard() {
                             className='event-CheckBox'
                             value="SocialMedia"
                             checked={checkedBoxes.includes('SocialMedia')}
-                            onChange={handleCheckboxChangeClick}
+                            onChange={handleCheckboxClick}
                         />
                         Social Media
                     </label>
@@ -608,7 +685,7 @@ function Dashboard() {
                             className='event-CheckBox'
                             value="LinkedIn"
                             checked={checkedBoxes.includes('LinkedIn')}
-                            onChange={handleCheckboxChangeClick}
+                            onChange={handleCheckboxClick}
                         />
                         LinkedIn
                     </label>
@@ -621,7 +698,7 @@ function Dashboard() {
                             className='event-CheckBox'
                             value="word Of Mouth"
                             checked={checkedBoxes.includes('word Of Mouth')}
-                            onChange={handleCheckboxChangeClick}
+                            onChange={handleCheckboxClick}
                         />
                         Word of Month
                     </label>
@@ -634,31 +711,39 @@ function Dashboard() {
                             className='event-CheckBox'
                             value="Whatsapp"
                             checked={checkedBoxes.includes('Whatsapp')}
-                            onChange={handleCheckboxChangeClick}
+                            onChange={handleCheckboxClick}
                         />
                         whatsapp
                     </label>
                     </div>
                     
                     <label htmlFor="form">Did you attend last year's Blue Economy Summit?</label>
+                    <span>{errorMessages.attendLastYear}</span>
                     <select name="attendLastYear" id="attend" onChange={handleChange}>
+                        <option value=""></option>
                         <option value="Yes">Yes</option>
                         <option value="No">No</option>
                     </select>
                     <label htmlFor="form">Which areas are of interest to you during the summit?</label>
+                    <span>{errorMessages.areaOfInterests}</span>
                     <select name="areaOfInterests" id="interests-area" onChange={handleChange}>
+                        <option value=""></option>
                         <option value="blue economy">Blue Economy</option>
                         <option value="climate change">Climate Change</option>
                         <option value="digital economy">Digital Economy</option>
                         <option value="all if possible">All if Possible</option>
                     </select>
                     <label htmlFor="form">Do you consent joining our mailing list to receive our newsletter?</label>
+                    <span>{errorMessages.joinMailList}</span>
                     <select name="joinMailList" id="join-mail" onChange={handleChange}>
+                        <option value=""></option>
                         <option value="Yes">Yes</option>
                         <option value="No">No</option>
                     </select>
                     <label htmlFor="form">How will you be joining this year's summit?</label>
+                    <span>{errorMessages.JoinAs}</span>
                     <select name="JoinAs" id="join-summit" onChange={handleChange}>
+                        <option value=""></option>
                         <option value="Startup">Start Up</option>
                         <option value="Delegate">Delegate</option>
                         <option value="Government">Government</option>
@@ -666,17 +751,22 @@ function Dashboard() {
                         <option value="Sponsor/Donor"></option>
                     </select>
                     <label htmlFor="form">Describe your product or the services that you offer?</label>
+                    <span>{errorMessages.describeYourProduct}</span>
                     <input type="text"
                     name='describeYourProduct'
                     value={formData.describeYourProduct}
                     onChange={handleChange}
                      />
                     <label htmlFor="form">Which category do you fall in?</label>
+                    <span>{errorMessages.categoryFall}</span>
                     <select name="categoryFall" onChange={handleChange}>
+                        <option value=""></option>
                         <option value="StartUp(KES 5000)">StartUp(KES 5000)</option>
                         <option value="Corporate Institution (KES 30,000)">Corporate Institution (KES 30,000)</option>
                     </select>
-                    <button type='submit'>Submit</button>
+                    <div className="modal-button">
+                            <button type='submit'>Register</button>
+                    </div> 
                 </form>
                     </div>
                     </div>
